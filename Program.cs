5cf12746a970d9cae8f0ff925c30a0d00483d78c
Kirill <ad777miral@gmail.com>
2023-01-15 14:23:20 +0300

    Zapolnenie massiva

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
 ﻿
+void Print(string[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+string[] Words(int size)
+{
+    string[] arr = new string[size];
+    Console.WriteLine("Введите текстовые элементы массива:");
+
+    for (int i = 0; i < size; i++)
+        arr[i] = Console.ReadLine();
+    return arr;
+}
+Console.WriteLine("Введите размер массива:");
+string[] arr_1 = Words(int.Parse(Console.ReadLine()));
+Print(arr_1);
+
+
+
+
